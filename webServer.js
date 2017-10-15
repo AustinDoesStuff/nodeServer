4cc47e592f74b87f393c5425b02cc5cfa80d0753
Austin Tanner <austin.tanner04@gmail.com>
2017-10-15 18:08:13 -0500

    added a few paths

--- webServer.js
+++ webServer.js
@@ -1,1 +1,22 @@
 const express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) =>{
+    res.send({
+        name: 'Austin',
+        likes: ['Mable', 'Morgan', 'Pizza']
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.send('This is the about page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({errorMessage: "you dun effed up"});
+});
+
+app.listen(3000);
